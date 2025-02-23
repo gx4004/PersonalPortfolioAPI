64483d8ba5499fd50496effafcc0724e9b513212
gx4004 <goncuegemen@gmail.com>
2025-02-23 21:16:07 +0100

    Remove all files

--- RiderProjects/km104/PersonalPortfolioAPI/PersonalPortfolio.API/Models/DTOs/SkillDto.cs
+++
@@ -1,8 +0,0 @@
-namespace PersonalPortfolio.API.Models.DTOs;
-
-public class SkillDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; } 
-    public string Proficiency { get; set; } 
-}
